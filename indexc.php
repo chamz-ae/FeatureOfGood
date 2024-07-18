1ca2359a5a081acd38b063887a2cde38cc6c54bc
icamanaksholeh <124222611+chamz-ae@users.noreply.github.com>
2024-07-18 08:54:46 +0700

    new feature

---
+++ indexc.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <title>Social Icons</title>
+  <link rel="stylesheet" href="style6.css" />
+</head>
+<body>
+  <div class="tooltip-container">
+    <div class="tooltip">
+      <div class="profile">
+        <div class="user">
+          <img src="https://res.cloudinary.com/dte2e4jx7/image/upload/v1721199840/ft9gmffkgi2ive5y4llz.jpg" class="img"></>
+          <div class="details">
+            <div class="name">IcamNotYours</div>
+            <div class="username">@хишам</div>
+          </div>
+        </div>
+        <div class="about">908 Followers</div>
+      </div>
+    </div>
+    <div class="text">
+      <a class="icon" href="https://www.instagram.com/icamnotyours">
+        <div class="layer">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span class="instagramSVG">
+          <svg fill="#000000" width="90px" height="50px" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+  <path d="M349.33 69.33H162.67C114.73 69.33 74.67 109.39 74.67 157.33v186.67c0 47.94 40.06 88 88 88h186.67c47.94 0 88-40.06 88-88V157.33c0-47.94-40.06-88-88-88zm61.33 236.67c0 33.87-27.46 61.33-61.33 61.33H162.67c-33.87 0-61.33-27.46-61.33-61.33V157.33c0-33.87 27.46-61.33 61.33-61.33h186.67c33.87 0 61.33 27.46 61.33 61.33v148.67z"/>
+  <path d="M256 149.33c-58.89 0-106.67 47.78-106.67 106.67s47.78 106.67 106.67 106.67 106.67-47.78 106.67-106.67S314.89 149.33 256 149.33zm0 170.67c-35.29 0-64-28.71-64-64s28.71-64 64-64 64 28.71 64 64-28.71 64-64 64z"/>
+  <circle cx="390.67" cy="121.33" r="21.33"/>
+</svg>
+          </span>
+        </div>
+        <div class="text">Instagram</div>
+      </a>
+    </div>
+  </div>
+</body>
+</html>
